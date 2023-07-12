2469395175ba0151dd78e53a407629ef23b746f4
Xiao Huang <huangxi@lxplus754.cern.ch>
2023-07-12 23:45:35 +0200

    rm cluster compatability, centrality, event plane

--- VertexCompositeProducer/test/PbPbSkimAndTree_PhaseII_DiMuContBothGammaGamma_mc_cfg.py
+++ VertexCompositeProducer/test/PbPbSkimAndTree_PhaseII_DiMuContBothGammaGamma_mc_cfg.py
@@ -37,46 +37,6 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', '')
 
-# Add PbPb centrality
-# centrality unavailable and do not use them
-process.load("RecoHI.HiCentralityAlgos.CentralityBin_cfi")
-process.centralityBin.Centrality = cms.InputTag("hiCentrality")
-process.centralityBin.centralityVariable = cms.string("HFtowers")
-process.GlobalTag.snapshotTime = cms.string("9999-12-31 23:59:59.000")
-process.GlobalTag.toGet.extend([
-    cms.PSet(record = cms.string("HeavyIonRcd"),
-        tag = cms.string("CentralityTable_HFtowers200_HydjetDrum5F_v1032x02_mc"),
-        connect = cms.string("frontier://FrontierProd/CMS_CONDITIONS"),
-        label = cms.untracked.string("HFtowers")
-        ),
-    ])
-process.cent_seq = cms.Sequence(process.centralityBin)
-
-# Add PbPb event plane
-# event plane unavailable and do not use them
-process.load("RecoHI.HiEvtPlaneAlgos.HiEvtPlane_cfi")
-process.load("RecoHI.HiEvtPlaneAlgos.hiEvtPlaneFlat_cfi")
-process.hiEvtPlane.trackTag = cms.InputTag("generalTracks")
-process.hiEvtPlane.vertexTag = cms.InputTag("offlinePrimaryVertices")
-process.hiEvtPlane.loadDB = cms.bool(True)
-process.hiEvtPlane.useNtrk = cms.untracked.bool(False)
-process.hiEvtPlane.caloCentRef = cms.double(-1)
-process.hiEvtPlane.caloCentRefWidth = cms.double(-1)
-process.hiEvtPlaneFlat.caloCentRef = cms.double(-1)
-process.hiEvtPlaneFlat.caloCentRefWidth = cms.double(-1)
-process.hiEvtPlaneFlat.vertexTag = cms.InputTag("offlinePrimaryVertices")
-process.hiEvtPlaneFlat.useNtrk = cms.untracked.bool(False)
-process.CondDB.connect = "sqlite_file:HeavyIonRPRcd_PbPb2018_offline.db"
-process.PoolDBESSource = cms.ESSource("PoolDBESSource",
-                                       process.CondDB,
-                                       toGet = cms.VPSet(cms.PSet(record = cms.string('HeavyIonRPRcd'),
-                                                                  tag = cms.string('HeavyIonRPRcd_PbPb2018_offline')
-                                                                  )
-                                                         )
-                                      )
-process.es_prefer_flatparms = cms.ESPrefer('PoolDBESSource','')
-process.evtplane_seq = cms.Sequence(process.hiEvtPlane * process.hiEvtPlaneFlat)
-
 # Add the VertexComposite producer
 process.load("VertexCompositeAnalysis.VertexCompositeProducer.generalDiMuCandidates_cff")
 process.generalDiMuCandidatesWrongSign = process.generalDiMuCandidates.clone(isWrongSign = cms.bool(True))
@@ -85,12 +45,10 @@ doPATMuons(process, True)
 
 # Add PbPb collision event selection
 process.load('VertexCompositeAnalysis.VertexCompositeProducer.collisionEventSelection_cff')
-process.load('VertexCompositeAnalysis.VertexCompositeProducer.clusterCompatibilityFilter_cfi')
 process.load('VertexCompositeAnalysis.VertexCompositeProducer.hfCoincFilter_cff')
-process.colEvtSel = cms.Sequence(process.hfCoincFilter2Th4 * process.primaryVertexFilterAA * process.clusterCompatibilityFilter)
+process.colEvtSel = cms.Sequence(process.hfCoincFilter2Th4 * process.primaryVertexFilterAA )
 
 # Define the analysis steps
-process.pcentandep_step = cms.Path(process.cent_seq * process.evtplane_seq)
 process.dimurereco_step = cms.Path(process.patMuonSequence * process.generalDiMuCandidates)
 process.dimurerecowrongsign_step = cms.Path(process.patMuonSequence * process.generalDiMuCandidatesWrongSign)
 
@@ -107,7 +65,6 @@ process.dimucontana_wrongsign_mc.doMuonNtuple = cms.untracked.bool(True)
 
 # Define the process schedule
 process.schedule = cms.Schedule(
-    process.pcentandep_step,
     process.dimurereco_step,
     process.dimurerecowrongsign_step,
     process.p
@@ -117,7 +74,6 @@ process.schedule = cms.Schedule(
 process.Flag_colEvtSel = cms.Path(process.colEvtSel)
 process.Flag_hfCoincFilter2Th4 = cms.Path(process.hfCoincFilter2Th4)
 process.Flag_primaryVertexFilter = cms.Path(process.primaryVertexFilterAA)
-process.Flag_clusterCompatibilityFilter = cms.Path(process.clusterCompatibilityFilter)
-eventFilterPaths = [ process.Flag_colEvtSel , process.Flag_hfCoincFilter2Th4 , process.Flag_primaryVertexFilter , process.Flag_clusterCompatibilityFilter ]
+eventFilterPaths = [ process.Flag_colEvtSel , process.Flag_hfCoincFilter2Th4 , process.Flag_primaryVertexFilter ]
 for P in eventFilterPaths:
     process.schedule.insert(0, P)
